fc8a074e6279f9ca50813dc3241ccf6abb6c7520
KrishKoria <krishkoria2004@gmail.com>
2024-04-02 00:04:35 +0530

    Added Some functionality to the viewModel and implemented the CustomTextField to the screen

--- app/src/main/java/com/example/mywishlistapp/AddEditDetails.kt
+++ app/src/main/java/com/example/mywishlistapp/AddEditDetails.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
@@ -17,9 +18,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 @Composable
@@ -44,6 +47,35 @@ fun AddEditDetails(
             verticalArrangement = Arrangement.Center
         ) {
             Spacer(modifier = Modifier.height(10.dp))
+            CustomTextField(
+                label = "Title",
+                value = viewModel.wishTitleState,
+                onValueChange = { viewModel.onWishTitleChange(it) }
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            CustomTextField(
+                label = "Description",
+                value = viewModel.wishDescriptionState,
+                onValueChange = { viewModel.onWishDescriptionChange(it) }
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Button(onClick = {
+                if (viewModel.wishTitleState.isNotEmpty() && viewModel.wishDescriptionState.isNotEmpty()) {
+//                    viewModel.updateWish(id)
+                } else {
+//                    viewModel.addWish()
+                }
+                navController.popBackStack()
+            }) {
+                Text(
+                    text = if (id != 0L) stringResource(id = R.string.edit_wish) else stringResource(
+                        id = R.string.add_wish
+                    ),
+                    style = TextStyle(
+                        fontSize = 18.sp,
+                    )
+                )
+            }
 
         }
 
@@ -68,7 +100,6 @@ fun CustomTextField(
             cursorColor = Color.Black,
             focusedLabelColor = Color.Black,
             unfocusedLabelColor = Color.Black
-
         )
     )
 }
