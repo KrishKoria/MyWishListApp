3e0eea9a72b73150124d77c186f3c53f3ef766fa
KrishKoria <krishkoria2004@gmail.com>
2024-04-02 23:11:47 +0530

    Started Work on Added Room Database for storing Wishes

---
+++ app/src/main/java/com/example/mywishlistapp/WishDAO.kt
@@ -0,0 +1,27 @@
+package com.example.mywishlistapp
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.mywishlistapp.data.Wish
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+abstract class WishDAO {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    abstract suspend fun addWish(wish: Wish)
+    @Query("SELECT * FROM `wishes-table`")
+    abstract fun getWishes(): Flow<List<Wish>>
+
+    @Update
+    abstract suspend fun updateWish(wish: Wish)
+
+    @Delete
+    abstract suspend fun deleteWish(wish: Wish)
+
+    @Query("SELECT * FROM `wishes-table` WHERE id = :id")
+    abstract fun getWishById(id: Long): Flow<Wish>
+}
--- app/src/main/java/com/example/mywishlistapp/data/Wish.kt
+++ app/src/main/java/com/example/mywishlistapp/data/Wish.kt
@@ -1,8 +1,16 @@
 package com.example.mywishlistapp.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "wishes-table")
 data class Wish (
+    @PrimaryKey(autoGenerate = true)
     val id: Long = 0L,
+    @ColumnInfo(name = "wish_title")
     val title: String = "",
+    @ColumnInfo(name = "wish_description")
     val description: String = "",
 )
 
